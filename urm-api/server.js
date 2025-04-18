bebb87217f641c46836ccab505114c6506c14691
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-04-18 23:19:46 +1000

    Setup basic server

--- urm-api/server.js
+++ urm-api/server.js
@@ -1,1 +1,17 @@
-// Server Setup Code here
+import express from "express";
+import cors from "cors";
+
+// initialize express app and PORT
+const app = express();
+const PORT = 3000;
+
+// middlewares
+app.use(cors()); // For Cross Origin Requests
+app.use(express.json()) // parse incoming requests with JSON payloads
+
+// Start Server and Listen to Request
+app.listen(PORT, (error) => {
+    error
+        ? console.log("Server started at port:"+PORT)
+        : console.log("Cannot start server !!! \n Error:"+error);
+})
