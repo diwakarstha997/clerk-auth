f962961655fe227131350b578314f6d8fbfef080
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-05-01 15:52:16 +1000

    Add Clerk Sign Up URL env and signup route

--- clerk-auth-client/src/App.jsx
+++ clerk-auth-client/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/auth/login" element={<SignInPage/>}/>
+        <Route path="/auth/signup" element={<>Sign Up Page</>}/>
       </Routes>
     </>
   )
--- clerk-auth-client/src/main.jsx
+++ clerk-auth-client/src/main.jsx
@@ -12,8 +12,9 @@ import './index.css'
 // Import Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-// Import clerk sign in related environment
+// Import clerk auth related environment
 const SIGN_IN_URL = import.meta.env.VITE_CLERK_SIGN_IN_URL
+const SIGN_UP_URL = import.meta.env.VITE_CLERK_SIGN_UP_URL
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
@@ -24,6 +25,7 @@ createRoot(document.getElementById('root')).render(
     <ClerkProvider 
       publishableKey={PUBLISHABLE_KEY} 
       signInUrl={SIGN_IN_URL}
+      signUpUrl={SIGN_UP_URL}
       afterSignOutUrl="/">
       <BrowserRouter>
         <App />
